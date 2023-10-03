bb892aa29fc2aecac2583dbc3564af82cfe741ed
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-03 10:51:22 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "My first App 2023"
+        Me.BackColor = Color.Blue
+        Me.MaximizeBox = True
+        Me.MinimizeBox = True
+        Me.Size = New Size(800, 1000)
+        Me.Opacity = 0.85 'makes it a bit transparent
+        Me.CenterToParent() 'centers the execution box
+    End Sub
+End Class
